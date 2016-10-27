68be167aaec253842c53184df659ce2ea72d232c
wujingpeng <wujingpeng@youzan.com>
2016-10-27 18:02:56 +0800

    Initialize the project

---
+++ SingleBarNavigationController/SingleBarNavigationController/Support Files/SingleBarNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  SingleBarNavigationController.h
+//  SingleBarNavigationController
+//
+//  Created by Broccoli on 2016/10/27.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SingleBarNavigationController.
+FOUNDATION_EXPORT double SingleBarNavigationControllerVersionNumber;
+
+//! Project version string for SingleBarNavigationController.
+FOUNDATION_EXPORT const unsigned char SingleBarNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SingleBarNavigationController/PublicHeader.h>
+
+
