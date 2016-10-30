9b72c5fe11b2dfadbcf70cd84064ff20471caa24
wujingpeng <wujingpeng@youzan.com>
2016-10-30 09:25:48 +0800

    Update Extension

--- SingleBarNavigationController/SingleBarNavigationController/Resouce/Extension.swift
+++ SingleBarNavigationController/SingleBarNavigationController/Resouce/Extension.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 broccoliii. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension Array {
     func any(_ condition: (Element) -> Bool) -> Bool {
@@ -17,5 +17,52 @@ extension Array {
         }
         
         return false
+    }
+}
+
+fileprivate var kInteractivePopGestureKey = "kInteractivePopGestureKey"
+
+extension UIViewController {
+    public var exclusiveNavigationController: ExclusiveNavigationController? {
+        if !(self is ExclusiveNavigationController) {
+            return self.navigationController as? ExclusiveNavigationController
+        }
+        return self as? ExclusiveNavigationController
+    }
+    
+    @IBInspectable public var disableInteractivePopGesture: Bool {
+        get {
+            guard let disableInteractivePopGesture = getAssociatedObject(&kInteractivePopGestureKey) else {
+                return false
+            }
+            return disableInteractivePopGesture as! Bool
+        }
+        
+        set {
+            setAssociatedObject(newValue as AnyObject?, associativeKey: &kInteractivePopGestureKey, policy: .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    public var navigationBarClass: AnyClass? {
+        return nil
+    }
+    
+    open func customBackItem(withTarget target: Any, action: Selector) -> UIBarButtonItem {
+        return UIBarButtonItem(title: NSLocalizedString("Back", comment: ""), style: .plain, target: target, action: action)
+    }
+}
+
+extension NSObject {
+    func setAssociatedObject(_ value: AnyObject?, associativeKey: UnsafeRawPointer, policy: objc_AssociationPolicy) {
+        if let valueAsAnyObject = value {
+            objc_setAssociatedObject(self, associativeKey, valueAsAnyObject, policy)
+        }
+    }
+    
+    func getAssociatedObject(_ associativeKey: UnsafeRawPointer) -> Any? {
+        guard let valueAsType = objc_getAssociatedObject(self, associativeKey) else {
+            return nil
+        }
+        return valueAsType
     }
 }
